c7e76dc731ff792e8afe7f682f71c8146155c41d
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2021-12-09 21:12:45 +0700

    major UI Update

--- Script/main.js
+++ Script/main.js
@@ -4,9 +4,11 @@ function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("header_txt").style.fontSize = "30px";
     document.getElementById("header").style.padding = "10px";
-    document.getElementById("header").style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+    document.getElementById("header").style.backgroundColor = "rgb(0, 0, 0)";
+    document.getElementById("header_txt").style.left = "300px";
 
   } else {
+    document.getElementById("header_txt").style.left = "50%";
     document.getElementById("header_txt").style.fontSize = "80px";
     document.getElementById("header").style.paddingTop = "20vh";
     document.getElementById("header").style.backgroundColor = "transparent";
