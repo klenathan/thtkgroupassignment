d08d7533d804ad09405038d65c5f600cc1d21592
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2021-12-10 19:55:30 +0700

    UI Update
    - added footer
    - changed theme color to dark
    - added navigation bar
    - added Ideal Job Comparison tab
    -added Project Idea page but not yet linked

--- Script/main.js
+++ Script/main.js
@@ -3,15 +3,27 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("header_txt").style.fontSize = "30px";
-    document.getElementById("header").style.padding = "10px";
+    document.getElementById("header").style.padding = "0px";
     document.getElementById("header").style.backgroundColor = "rgb(0, 0, 0)";
     document.getElementById("header_txt").style.left = "300px";
 
-    document.getElementsByClassName("scrolldown").style.display = "none";
+    document.getElementById("menu").style.display = "flex"
   } else {
     document.getElementById("header_txt").style.left = "50%";
     document.getElementById("header_txt").style.fontSize = "80px";
     document.getElementById("header").style.paddingTop = "20vh";
     document.getElementById("header").style.backgroundColor = "transparent";
+
+    document.getElementById("menu").style.display = "none"
   }
+}
+
+function scrolltodiv(div) {
+  var elmnt = document.getElementById(div);
+  elmnt.scrollIntoView({ behavior: 'smooth', block: 'center' }, false);
+}
+
+
+function scrolltotop() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
 }
